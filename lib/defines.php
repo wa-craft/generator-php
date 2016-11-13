946d4b820f474dc5849d371ba290561ab669ceee
bison <1308362@gmail.com>
2016-11-13 18:24:01 +0800

    全局定义分离到 defines.php

---
+++ lib/defines.php
@@ -0,0 +1,10 @@
+<?php
+//定义基本数据
+define('TAR_ROOT_PATH', './deploy/');
+define('APP_PATH', 'application');
+define('DBFILE_PATH', TAR_ROOT_PATH . 'database/');
+define('PROFILE_PATH', TAR_ROOT_PATH . 'profile/');
+define('TMPL_PATH', './template');
+define('PUB_PATH', TAR_ROOT_PATH . '/public/');
+define('SHARE_PATH', './share');
+define('ROOT_REPOS', 'https://github.com/goldeagle/bforge-think');
