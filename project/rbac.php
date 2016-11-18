8b09f35df23f3593db61c36bde707077ef4fcf0b
bison <1308362@gmail.com>
2016-11-18 20:51:45 +0800

    修改 rbac 基本数据

--- project/rbac.php
+++ project/rbac.php
@@ -41,8 +41,11 @@ return [
                             'comment' => '用户',
                             'autoWriteTimeStamp' => true,
                             'fields' => [
-                                ['name' => 'name', 'title' => '名称', 'rule' => 'alpha', 'required' => true, 'is_unique' => false],
-                                ['name' => 'author_id', 'title' => '用户编号', 'rule' => 'number', 'required' => false, 'is_unique' => false]
+                                ['name' => 'account', 'title' => '帐号', 'rule' => 'alpha', 'required' => true, 'is_unique' => true],
+                                ['name' => 'password', 'title' => '密码', 'rule' => 'alpha', 'required' => true, 'is_unique' => false],
+                                ['name' => 'logo', 'title' => '头像地址', 'rule' => 'url', 'required' => false, 'is_unique' => false],
+                                ['name' => 'ip', 'title' => 'IP地址', 'rule' => 'ip', 'required' => false, 'is_unique' => false],
+                                ['name' => 'is_blocked', 'title' => '禁用', 'rule' => 'boolean', 'required' => true, 'is_unique' => false]
                             ],
                             'relations' => [
                                 [
@@ -68,17 +71,18 @@ return [
                             'comment' => '用户',
                             'autoWriteTimeStamp' => true,
                             'fields' => [
-                                ['name' => 'name', 'title' => '名称', 'rule' => 'alpha', 'required' => true, 'is_unique' => false],
-                                ['name' => 'author_id', 'title' => '用户编号', 'rule' => 'number', 'required' => false, 'is_unique' => false]
+                                ['name' => 'name', 'title' => '姓名', 'rule' => 'alpha', 'required' => true, 'is_unique' => false],
+                                ['name' => 'gender', 'title' => '性别', 'rule' => 'number', 'required' => true, 'is_unique' => false],
+                                ['name' => 'mobile', 'title' => '手机号码', 'rule' => 'number', 'required' => true, 'is_unique' => true]
                             ],
                             'relations' => [
                                 [
-                                    'name' => 'UserGroup',
-                                    'caption' => '所属分组',
+                                    'name' => 'User',
+                                    'caption' => '所属用户',
                                     'type' => 'belongsTo',
-                                    'this_key' => 'user_group_id',
+                                    'this_key' => 'user_id',
                                     'that_key' => 'id',
-                                    'model' => 'UserGroup'
+                                    'model' => 'User'
                                 ]
                             ]
                         ],
@@ -87,8 +91,7 @@ return [
                             'comment' => '用户分组',
                             'autoWriteTimeStamp' => true,
                             'fields' => [
-                                ['name' => 'name', 'title' => '名称', 'rule' => 'alpha', 'required' => true, 'is_unique' => false],
-                                ['name' => 'author_id', 'title' => '用户编号', 'rule' => 'number', 'required' => false, 'is_unique' => false]
+                                ['name' => 'name', 'title' => '名称', 'rule' => 'alpha', 'required' => true, 'is_unique' => false]
                             ],
                             'relations' => [
                                 [
@@ -106,8 +109,7 @@ return [
                             'comment' => '角色',
                             'autoWriteTimeStamp' => false,
                             'fields' => [
-                                ['name' => 'name', 'title' => '名称', 'rule' => 'alpha', 'required' => true, 'is_unique' => false],
-                                ['name' => 'author_id', 'title' => '用户编号', 'rule' => 'number', 'required' => false, 'is_unique' => false]
+                                ['name' => 'name', 'title' => '名称', 'rule' => 'alpha', 'required' => true, 'is_unique' => false]
                             ],
                             'relations' => [
                                 [
@@ -125,16 +127,24 @@ return [
                             'comment' => '节点',
                             'autoWriteTimeStamp' => false,
                             'fields' => [
-                                ['name' => 'name', 'title' => '名称', 'rule' => 'alpha', 'required' => true, 'is_unique' => false],
-                                ['name' => 'author_id', 'title' => '用户编号', 'rule' => 'number', 'required' => false, 'is_unique' => false]
+                                ['name' => 'caption', 'title' => '名称', 'rule' => 'chsAlpha', 'required' => false, 'is_unique' => false],
+                                ['name' => 'type', 'title' => '类型', 'rule' => 'number', 'required' => false, 'is_unique' => false]
                             ],
                             'relations' => [
                                 [
                                     'name' => 'Parent',
                                     'caption' => '父节点',
-                                    'type' => 'belongsToMany',
+                                    'type' => 'belongsTo',
                                     'this_key' => 'parent_id',
                                     'that_key' => 'id',
+                                    'model' => 'Node'
+                                ],
+                                [
+                                    'name' => 'Children',
+                                    'caption' => '子节点',
+                                    'type' => 'hasMany',
+                                    'this_key' => 'id',
+                                    'that_key' => 'parent_id',
                                     'model' => 'Node'
                                 ]
                             ]
