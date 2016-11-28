be404cb803f104f6aef785fdbe5d7b72f3725028
goldeagle <1308362@gmail.com>
2016-11-28 22:47:50 +0800

    修改：通过遍历的方法装载所有指定目录下的 php 文件

--- src/autoload.php
+++ src/autoload.php
@@ -2,14 +2,29 @@
 /**
  * 自动装载程序
  */
-//TODO 使用 generator 遍历目录下的所有 php 文件，并包含
-$src_path = SRC_PATH . '/lib';
-$files = scandir($src_path);
-foreach ($files as $file) {
+$src_path = SRC_PATH . '/thinkbuilder';
+foreach (scan($src_path) as $file) {
     $src_file_name = $src_path . '/' . $file;
-    if (is_file($src_file_name)) {
-        if (preg_match('/\.php$/', $src_file_name)) {
-            require $src_file_name;
+    if (is_file($file)) {
+        if (preg_match('/\.php$/', $file)) {
+            require $file;
+        }
+    }
+}
+
+/**
+ * 迭代遍历并返回指定目录下的所有文件（非目录）
+ * @param string $dir 指定的目录
+ * @return array Generator 生成器生成的结果
+ */
+function scan($dir)
+{
+    $files = scandir($dir);
+    foreach ($files as $file) {
+        if (is_dir("$dir/$file")) {
+            if (preg_match('/^\./', $file) === 0) yield from scan("$dir/$file");
+        } else {
+            yield "$dir/$file";
         }
     }
 }
