63f0a60463cbdfbcf4890e9e5bf7e1677b240a61
goldeagle <1308362@gmail.com>
2016-11-28 23:43:25 +0800

    修改：自动装载 Class

--- src/autoload.php
+++ src/autoload.php
@@ -2,15 +2,24 @@
 /**
  * 自动装载程序
  */
+
+//自动装载文件中的类
 $src_path = SRC_PATH . '/thinkbuilder';
-foreach (scan($src_path) as $file) {
-    $src_file_name = $src_path . '/' . $file;
+
+foreach (scan($src_path) as $class => $file) {
     if (is_file($file)) {
         if (preg_match('/\.php$/', $file)) {
-            require $file;
+            spl_autoload($class);
         }
     }
 }
+
+/**
+ * 类的自动注册程序
+ */
+spl_autoload_register(function ($class) {
+    include classNameToPath($class);
+});
 
 /**
  * 迭代遍历并返回指定目录下的所有文件（非目录）
@@ -24,7 +33,32 @@ function scan($dir)
         if (is_dir("$dir/$file")) {
             if (preg_match('/^\./', $file) === 0) yield from scan("$dir/$file");
         } else {
-            yield "$dir/$file";
+            yield pathToClassName("$dir/$file") => "$dir/$file";
         }
     }
+}
+
+/**
+ * 把类文件的路径转换为类名
+ * @param $path
+ * @return mixed
+ */
+function pathToClassName($path)
+{
+    $class = str_replace('.php', '', $path);
+    $class = str_replace(SRC_PATH . '/', '', $class);
+    $class = str_replace('/', '\\', $class);
+    return $class;
+}
+
+/**
+ * 把类名转换为文件名
+ * @param $class
+ * @return mixed|string
+ */
+function classNameToPath($class)
+{
+    $path = str_replace('\\', '/', $class);
+    $path = SRC_PATH . '/' . $path . '.php';
+    return $path;
 }
