96c4951d35e82bbb66659740a8198a00e81a0bea
goldeagle <1308362@gmail.com>
2016-11-30 21:59:14 +0800

    增加：拷贝与写入的trait

---
+++ src/thinkbuilder/traits/CopyTrait.php
@@ -0,0 +1,14 @@
+<?php
+namespace thinkbuilder\traits;
+
+/**
+ * Class CopyTrait 拷贝trait
+ * @package thinkbuilder\traits
+ */
+trait CopyTrait
+{
+    public function copy()
+    {
+
+    }
+}
---
+++ src/thinkbuilder/traits/WriteTrait.php
@@ -0,0 +1,14 @@
+<?php
+namespace thinkbuilder\traits;
+
+/**
+ * Class FileWriter 文件写入器
+ * @package thinkbuilder\traits
+ */
+trait WriteTrait
+{
+    public function writeToFile()
+    {
+
+    }
+}
