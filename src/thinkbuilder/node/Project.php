5705599fca7402b8e601ea97e687ac128a2c2915
goldeagle <1308362@gmail.com>
2016-12-03 17:40:22 +0800

    增加：处理方法

--- src/thinkbuilder/node/Project.php
+++ src/thinkbuilder/node/Project.php
@@ -1,6 +1,9 @@
 <?php
 namespace thinkbuilder\node;
 
+use thinkbuilder\generator\Generator;
+use thinkbuilder\helper\TemplateHelper;
+
 /**
  * Class Project
  * @package thinkbuilder\node
@@ -11,4 +14,32 @@ class Project extends Node
     protected $domain = '';
     //项目下的应用
     protected $applications = [];
+
+    public function process()
+    {
+        //生成 nginx 配置文件
+        if ($this->config['actions']['nginx']) {
+            Generator::create('Profile', [
+                    'type' => 'nginx',
+                    'path' => $this->paths['profile'],
+                    'file_name' => '/nginx_vhost',
+                    'template' => TemplateHelper::fetchTemplate('nginx'),
+                    'project' => $this->data
+                ]
+            )->generate()->writeToFile();
+        }
+
+        //生成 apache htaccess 配置文件
+        if ($this->config['actions']['apache']) {
+            Generator::create('Profile', [
+                'type' => 'apache',
+                'path' => $this->paths['public'],
+                'file_name' => '/.htaccess',
+                'template' => TemplateHelper::fetchTemplate('apache'),
+                'project' => $this->data
+            ])->generate()->writeToFile();
+        }
+
+        $this->processChildren('application');
+    }
 }
