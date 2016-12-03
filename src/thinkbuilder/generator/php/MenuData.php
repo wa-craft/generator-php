7c5bad52d37a6bf12c89a1cf41f38e6920c6879a
goldeagle <1308362@gmail.com>
2016-12-03 20:20:05 +0800

    增加：菜单数据生成器

---
+++ src/thinkbuilder/generator/php/MenuData.php
@@ -0,0 +1,13 @@
+<?php
+namespace thinkbuilder\generator\php;
+
+use thinkbuilder\generator\Generator;
+
+class MenuData extends Generator
+{
+    public function generate(): Generator
+    {
+        // TODO: Implement generate() method.
+        return $this;
+    }
+}
