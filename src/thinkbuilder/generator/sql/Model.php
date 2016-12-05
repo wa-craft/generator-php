00d4c605b35bfad0d86d4f0e4c5c508a8ceba608
goldeagle <1308362@gmail.com>
2016-12-05 20:38:37 +0800

    增加：field 字段处理方法

--- src/thinkbuilder/generator/sql/Model.php
+++ src/thinkbuilder/generator/sql/Model.php
@@ -2,10 +2,12 @@
 namespace thinkbuilder\generator\sql;
 
 use thinkbuilder\generator\Generator;
-use thinkbuilder\helper\{
-    ClassHelper, TemplateHelper
-};
+use thinkbuilder\helper\TemplateHelper;
 
+/**
+ * Class Model 基于模型的 sql 代码生成器
+ * @package thinkbuilder\generator\sql
+ */
 class Model extends Generator
 {
     public function generate(): Generator
@@ -18,16 +20,16 @@ class Model extends Generator
         if (isset($data['fields'])) {
             $fields = $data['fields'];
             foreach ($fields as $field) {
-                $content_field = '  ' . TemplateHelper::getFieldSQL($field);
-                $content_field = str_replace('{{FIELD_NAME}}', $field->name, $content_field);
-                $content_field = str_replace('{{FIELD_TITLE}}', $field->title, $content_field);
+                $content_field = "\t" . self::getFieldSQL($field);
+                $content_field = str_replace('{{FIELD_NAME}}', $field['name'], $content_field);
+                $content_field = str_replace('{{FIELD_TITLE}}', $field['title'], $content_field);
                 $content = str_replace('{{FIELD_LOOP}}', $content_field . "\n{{FIELD_LOOP}}", $content);
             }
         }
 
         $tags = [
             'NAME_SPACE' => $data['namespace'],
-            'MODEL_NAME' => ClassHelper::convertToTableName($data['name'], $data['namespace']),
+            'MODEL_NAME' => $this->params['model_name'],
             'MODEL_COMMENT' => $data['caption'],
             'CONTROLLER_PARAMS' => $content_field,
             'CLASS_NAME' => $data['name'],
@@ -35,8 +37,41 @@ class Model extends Generator
             'MODULE_NAME' => $data['namespace'],
             'FIELD_LOOP' => '',
         ];
-        $this->content = TemplateHelper::parseTemplateTags($tags, $this->params['template']);
+        $this->content = TemplateHelper::parseTemplateTags($tags, $content);
 
         return $this;
+    }
+
+    /**
+     * 判断字段的类型和参数，生成不同类型的字段 sql
+     * @param $field
+     * @return string
+     */
+    public static function getFieldSQL($field)
+    {
+        //字段是否必须
+        $null_string = (isset($field->required)) ? ($field->required ? ' NOT NULL ' : '') : '';
+
+        if (preg_match('/_id$/', $field['name'])) {
+            return "`{{FIELD_NAME}}` bigint(20) $null_string COMMENT '{{FIELD_TITLE}}',";
+        }
+
+        if (preg_match('/^is_/', $field['name'])) {
+            $default = (array_key_exists('default', $field)) ? ($field->default ? '\'1\'' : '\'0\'') : '\'0\'';
+            return "`{{FIELD_NAME}}` tinyint(1) DEFAULT $default COMMENT '{{FIELD_TITLE}}',";
+        }
+
+        if ($field['rule'] == 'datetime') {
+            return "`{{FIELD_NAME}}` datetime $null_string DEFAULT CURRENT_TIMESTAMP COMMENT '{{FIELD_TITLE}}',";
+        }
+
+        if ($field['rule'] == 'text') {
+            return "`{{FIELD_NAME}}` TEXT COMMENT '{{FIELD_TITLE}}',";
+        }
+
+        $default = (array_key_exists('default', $field)) ? ' DEFAULT \'' . $field->default . '\' ' : ' DEFAULT NULL';
+        $null_string = $default !== '' ? $default : $null_string;
+
+        return "`{{FIELD_NAME}}` varchar(100) $null_string COMMENT '{{FIELD_TITLE}}',";
     }
 }
