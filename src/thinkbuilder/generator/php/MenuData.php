8bab05045b69edff076dab6d285c7a9f9a4d4148
goldeagle <1308362@gmail.com>
2016-12-08 16:20:08 +0800

    修改：修改生成菜单的 icon

--- src/thinkbuilder/generator/php/MenuData.php
+++ src/thinkbuilder/generator/php/MenuData.php
@@ -13,10 +13,10 @@ class MenuData extends Generator
     public function generate(): Generator
     {
         $data = $this->params['data'];
-        $items = "\t['title' => '{$data['caption']}', 'url' => '', 'icon' => 'fa-folder', 'sub_menu' =>" . PHP_EOL;
+        $items = "\t['title' => '{$data['caption']}', 'url' => '', 'icon' => 'icon-folder', 'sub_menu' =>" . PHP_EOL;
         $items .= " \t\t[" . PHP_EOL;
         foreach ($data['modules'] as $module) {
-            $items .= "\t\t['title' => '{$module['caption']}', 'url' => '', 'icon' => 'fa-folder', 'sub_menu' =>" . PHP_EOL;
+            $items .= "\t\t['title' => '{$module['caption']}', 'url' => '', 'icon' => 'icon-folder', 'sub_menu' =>" . PHP_EOL;
             $items .= " \t\t\t[" . PHP_EOL;
             //处理 model，并创建自动控制器
             $controllers = [];
@@ -27,7 +27,7 @@ class MenuData extends Generator
 
             //根据 controller 来生成菜单索引
             foreach ($controllers as $controller) {
-                $items .= "\t\t\t\t['title' => '{$controller['caption']}', 'url' => '{$module['name']}/{$controller['name']}/index', 'icon' => 'fa-folder',]," . PHP_EOL;
+                $items .= "\t\t\t\t['title' => '{$controller['caption']}', 'url' => '{$module['name']}/{$controller['name']}/index', 'icon' => 'icon-folder',]," . PHP_EOL;
             }
             $items .= "\t\t\t\t]" . PHP_EOL;
             $items .= "\t\t\t]," . PHP_EOL;
