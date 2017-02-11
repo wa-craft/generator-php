374a81055686801b07ae61472484f06e911cf642
goldeagle <1308362@gmail.com>
2017-02-11 15:21:46 +0800

    修改：当控制器类的父类为空时不再默认使用 think\Controller，并且在空方法中创建 $view 对象

--- src/thinkbuilder/generator/php/Controller.php
+++ src/thinkbuilder/generator/php/Controller.php
@@ -45,8 +45,8 @@ class Controller extends Generator
         })($data);
 
         //设置父控制器
-        $extend_controller = ($extend_controller !== '') ? 'extends ' . $extend_controller : (($data['name'] != 'Error') ? 'extends ' . $data['parent_controller'] : '');
-        if ($extend_controller == 'extends ') $extend_controller = 'extends \\think\\Controller';
+        $extend_controller = ($extend_controller !== '') ? ' extends ' . $extend_controller : (($data['name'] != 'Error') ? ' extends ' . $data['parent_controller'] : '');
+        if ($extend_controller == ' extends ') $extend_controller = '';
         $tags['EXTEND_CLASS'] = $extend_controller;
         $content = $this->params['template'];
 
