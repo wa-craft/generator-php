1aef974477881e5c0f365dbd411ee8441654083c
fake.coin <fake.coin@yahoo.com>
2018-12-04 12:05:24 +0800

    DEL: tmp dir

--- tmp/database/tf_relation.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `tf_relation`;
-CREATE TABLE IF NOT EXISTS `tf_relation` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-	`setting` varchar(100)  NOT NULL  COMMENT '设置',
-	`value` varchar(100)  NOT NULL  COMMENT '取值',
-	`author_id` bigint(20)  DEFAULT '0'  COMMENT '作者',
-
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='tf|index 设置表';
--- tmp/database/tf_setting.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `tf_setting`;
-CREATE TABLE IF NOT EXISTS `tf_setting` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-	`keyword` varchar(100)  DEFAULT ''  COMMENT '键',
-	`caption` varchar(100)  DEFAULT ''  COMMENT '说明',
-	`value` varchar(100)  DEFAULT ''  COMMENT '值',
-
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='tf|index 设置表';
