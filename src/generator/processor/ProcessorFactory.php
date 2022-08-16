234b28d2b754c6905e06dfc5806cb0b9c4c5bb81
Bison 'goldeagle' Fan <bison@bitseed.cn>
2022-08-16 21:57:17 +0800

    upd: patch checks

--- src/generator/processor/ProcessorFactory.php
+++ src/generator/processor/ProcessorFactory.php
@@ -17,13 +17,32 @@ final class ProcessorFactory
             $obj = ClassHelper::create("generator\\processor\\" . ucfirst($type_name));
 
             //创建路径
+            $_prj_src_path = '';
+            $_prj_tar_path = '';
             if ($obj instanceof Processor) {
                 $_name = strtolower($processor_type->name);
-                $src = ROOT_PATH . '/resource/' . $_name . '/' . ($cache->get('project')[$_name] ?: '') . '/src';
-                $tar = ($cache->get('target_paths')[$_name]) ? ROOT_PATH . '/' . $cache->get('target_paths')[$_name] : ROOT_PATH . '/deploy/' . $_name;
 
-                //拷贝基本文件
-                FileHelper::copyFiles($src, $tar);
+                //判断并设置原始路径
+                $project = $cache->get('project') ?: [];
+                if (!empty($project)) {
+                    $_prj_src_path = (array_key_exists($_name, $project)) ? $project[$_name] : '';
+                }
+
+                $target_paths = $cache->get('target_paths');
+                if (!empty($target_paths)) {
+                    $_prj_tar_path = (array_key_exists($_name, $target_paths)) ? $target_paths[$_name] : '';
+                }
+
+                if (empty($_prj_src_path) || empty($_prj_tar_path) || empty($cache->get('project')[$type_name])) {
+                    echo "WARNING: CANNOT find the source path or the target path of resouce \"{$_name}\"!" . PHP_EOL;
+                } else {
+                    $src = ROOT_PATH . '/resource/' . $_name . '/' . $_prj_src_path . '/src';
+                    $tar = $_prj_tar_path ? ROOT_PATH . '/' . $cache->get('target_paths')[$_name] : ROOT_PATH . '/deploy/' . $_name;
+
+                    //拷贝基本文件
+                    FileHelper::copyFiles($src, $tar);
+                }
+
                 return $obj;
             } else {
                 return null;
