7e5d16beb5df0c444f6455a28f1655b25e771b4a
Bison 'goldeagle' Fan <bison@bitseed.cn>
2022-08-23 16:10:26 +0800

    fix: template stereotype class name

--- src/generator/template/TemplateFactory.php
+++ src/generator/template/TemplateFactory.php
@@ -8,7 +8,11 @@ class TemplateFactory
 {
     public static function create(string $stereoType, array $params = []): StereoType|null
     {
-        $obj = ClassHelper::create("generator\\template\\" . $stereoType, $params);
+        list($lang, $type) = explode('/', $stereoType);
+        $obj = ClassHelper::create("generator\\template\\"
+            . strtolower($lang)
+            . "\\Stereotype"
+            . ucfirst($type), $params);
         //处理 schema 对象
         if (!empty($params)) {
         }
